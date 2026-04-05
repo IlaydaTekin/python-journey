be751a6cabd7620ada504064019a78e66fec4f00
IlaydaTekin <ilaydatekinnn123@gmail.com>
2026-04-05 23:41:36 +0300

    add README for number guessing game

---
+++ number-guessing-game/README.md/# Number Guessing Game  This is a simple Python game where the user tries to guess a randomly chosen number.  ## Features - User input handling - Loop logic - Conditional statements  ## How to run Run the Python file:  python secret_number.py
@@ -0,0 +1,12 @@
+# Number Guessing Game
+This is a simple Python game where the user tries to guess a randomly chosen number.
+
+## Features
+- User input handling
+- Loop logic
+- Conditional statements
+
+## How to run
+Run the Python file:
+
+python secret_number.py
